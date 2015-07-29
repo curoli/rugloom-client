82868641876f1ea5b316f18ef55e89e5c0ab9fa4
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-07-29 18:21:54 -0400

    Web socket dialog stub.

--- app/rugloom/web/controllers/RugLoomController.scala
+++ app/rugloom/web/controllers/RugLoomController.scala
@@ -1,15 +1,21 @@
 package rugloom.web.controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{WebSocket, Action, Controller}
 import rugloom.views.html.RugLoomView
+import play.api.Play.current
 
 /**
  * Created by oliverr on 7/29/2015.
+ * Controller for RugLoom webservices
  */
 object RugLoomController extends Controller {
 
   def index = Action {
     Ok(RugLoomView.index("RugLoom"))
+  }
+
+  def socket = WebSocket.acceptWithActor[String, String] { request => out =>
+    RugLoomSocketActor.props(out)
   }
 
 }
---
+++ app/rugloom/web/controllers/RugLoomSocketActor.scala
@@ -0,0 +1,18 @@
+package rugloom.web.controllers
+
+/**
+ * Created by oliverr on 7/29/2015.
+ * Actor to handle websocket communication
+ */
+import akka.actor._
+
+object RugLoomSocketActor {
+  def props(out: ActorRef) = Props(new RugLoomSocketActor(out))
+}
+
+class RugLoomSocketActor(out: ActorRef) extends Actor {
+  def receive = {
+    case msg: String =>
+      out ! ("I received your message: " + msg)
+  }
+}
