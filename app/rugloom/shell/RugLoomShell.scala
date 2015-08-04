845f23c5f10e386580080741359969cde3ed5905
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-08-04 11:40:18 -0400

    Finally fixed classpath to run IMain within Play.

--- app/rugloom/shell/RugLoomShell.scala
+++ app/rugloom/shell/RugLoomShell.scala
@@ -13,31 +13,12 @@ import play.api.Play.current
 class RugLoomShell {
 
   val settings = new Settings
-  //  val javaPath = (new File(System.getProperty("java.class.path"))).toURI
-  //  val compilerPath = java.lang.Class.forName("scala.tools.nsc.interpreter.IMain").getProtectionDomain.getCodeSource.getLocation
-  //  val libPath = java.lang.Class.forName("scala.Some").getProtectionDomain.getCodeSource.getLocation
-  val compilerPath = current.classloader.loadClass("scala.tools.nsc.interpreter.IMain").getProtectionDomain.getCodeSource.getLocation
-  val libPath = current.classloader.loadClass("scala.Some").getProtectionDomain.getCodeSource.getLocation
-
-  //  println("javaPath=" + javaPath);
-  println("compilerPath=" + compilerPath);
-  println("libPath=" + libPath);
-
-  //  settings.classpath.value = List(javaPath, compilerPath, libPath) mkString java.io.File.pathSeparator
-  //  settings.classpath.value =
-  //    "C:\\Program Files (x86)\\sbt\\bin\\sbt-launch.jar;C:\\Users\\oliverr\\.ivy2\\cache\\org.scala-lang\\scala-compiler\\jars\\scala-compiler-2.11.7.jar;C:\\Users\\oliverr\\.ivy2\\cache\\org.scala-lang\\scala-library\\jars\\scala-library-2.11.7.jar"
-  println("classpath: " + settings.classpath)
-  settings.usejavacp.value = true
-  println("classpath: " + settings.classpath)
-  settings.embeddedDefaults[RugLoomShell]
-  println("classpath: " + settings.classpath)
+  val sbtClasspath = System.getProperty("rugloom.interpreter.classpath")
+  println("rugloom.interpreter.classpath = " + sbtClasspath)
+  settings.classpath.value = "." + File.pathSeparator + sbtClasspath
   val imain = new IMain(settings) {
     override def parentClassLoader: ClassLoader = current.classloader
   }
-  imain.addUrlsToClassPath(compilerPath, libPath)
-
-  println("IMain class loader: " + imain.classLoader)
-  println("IMain compiler classpath: " + imain.compilerClasspath)
 
   def lineEntered(line: String): Unit = imain.interpret(line)
 
--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
+import java.io.File
+
 name := "rugloom-client"
 
 version := "1.0.0-SNAPSHOT"
@@ -8,5 +10,15 @@ libraryDependencies ++= Seq(
   "org.scala-lang" % "scala-library" % scalaVersion.value,
   "org.scala-lang" % "scala-compiler" % scalaVersion.value,
   "org.scala-lang" % "scala-reflect" % scalaVersion.value)
+
+lazy val sbtcp = taskKey[Unit]("sbt-classpath")
+
+sbtcp := {
+  val files: Seq[File] = (fullClasspath in Compile).value.files
+  val sbtClasspath : String = files.map(x => x.getAbsolutePath).mkString(File.pathSeparator)
+  println("Set SBT classpath to 'sbt-classpath' environment variable")
+  println(sbtClasspath)
+  System.setProperty("rugloom.interpreter.classpath", sbtClasspath)
+}
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
