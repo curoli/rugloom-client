fbf39de9614366bee0afd7359ac6304535ebc403
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-08-08 16:29:46 -0400

    Fixing strange inconsistency.

--- app/rugloom/rug/GenoType.scala
+++ app/rugloom/rug/GenoType.scala
@@ -4,6 +4,10 @@ package rugloom.rug
  * RugLoom - Explorative analysis pipeline prototype
  * Created by oliverr on 8/7/2015.
  */
-class GenoType {
+object Genotype {
+
+}
+
+case class Genotype(variation: Variation, zygosity: Int) {
 
 }
